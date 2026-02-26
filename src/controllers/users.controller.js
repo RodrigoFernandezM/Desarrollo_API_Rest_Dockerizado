aba52e19d3383065394f42fd681a0664afb7dfb3
rodrigo <rodrigo.fermer@iesjulianmarias.es>
2026-02-26 21:46:11 +0100

    Modificación 2 controlador Users.

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,54 +1,12 @@
-const User = require('../models/User');
+const express = require('express');
+const router = express.Router();
 
-// Crear usuario
-exports.createUser = async (req, res) => {
-    try {
-        const user = await User.create(req.body);
-        res.json(user);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
+const usersController = require('../controllers/users.controller');
 
-// Listar usuarios
-exports.getUsers = async (req, res) => {
-    const users = await User.find();
-    res.json(users);
-};
+router.post('/', usersController.createUser);
+router.get('/', usersController.getUsers);
+router.get('/:id', usersController.getUserById);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.deleteUser);
 
-// Obtener usuario por ID
-exports.getUserById = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).json({ mensaje: 'Usuario no encontrado' });
-        res.json(user);
-    } catch (error) {
-        res.status(400).json({ error: 'ID inválido' });
-    }
-};
-
-// Actualizar usuario
-exports.updateUser = async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true, runValidators: true }
-        );
-        if (!user) return res.status(404).json({ mensaje: 'Usuario no encontrado' });
-        res.json(user);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
-
-// Eliminar usuario
-exports.deleteUser = async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) return res.status(404).json({ mensaje: 'Usuario no encontrado' });
-        res.json({ mensaje: 'Usuario eliminado' });
-    } catch (error) {
-        res.status(400).json({ error: 'ID inválido' });
-    }
-};
+module.exports = router;
